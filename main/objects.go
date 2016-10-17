97f8487e448ac7897f591bf658e6a1fba1ef670d
Bernitt <j.b5286@gmail.com>
2016-10-17 11:16:02 -0400

    fixing row/column names

--- main/objects.go
+++ main/objects.go
@@ -53,7 +53,7 @@ type Column struct {
 }
 
 type Board struct {
-	A0, B0, C0, D0, E0, F0, G0, H0,
+	A8, B8, C8, D8, E8, F8, G8, H8,
 	A1, B1, C1, D1, E1, F1, G1, H1,
 	A2, B2, C2, D2, E2, F2, G2, H2,
 	A3, B3, C3, D3, E3, F3, G3, H3,
