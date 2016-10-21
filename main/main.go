c753dbdb5959e98d0d2bca6c435050bcfac365ac
joeb000 <j.b5286@gmail.com>
2016-10-20 22:51:33 -0400

    adding empty game at gid=1

--- main/main.go
+++ main/main.go
@@ -9,6 +9,9 @@ var hub = NewHub()
 
 func main() {
 
+	CreateGame(Player{})
+	JoinGame(Player{}, 1)
+
 	go hub.Run()
 	router := NewRouter()
 	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("../public/"))))
